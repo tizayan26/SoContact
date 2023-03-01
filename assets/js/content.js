42120046072079d57d14946b6da6c083b9408048
tizayan26 <zayan.upal@gmail.com>
2023-03-01 09:51:56 -0500

    version 1.0.0.3 fix

--- assets/js/content.js
+++ assets/js/content.js
@@ -128,7 +128,7 @@ function scrapeLinkedInProfile(type){
                         obj.validity = certification[i].children[0].children[1].children[0].children[0].children[2].children[0].innerText;
                         var dates_array = certification[i].children[0].children[1].children[0].children[0].children[2].children[0].innerText.trim().split('·');
                         var start_date = new Date(dates_array[0].trim());
-                        var end_date = dates_array[1].trim() == "No Expiration Date" ? new Date() : new Date(dates_array[1].trim());
+                        var end_date = dates_array[1] == "No Expiration Date" ? new Date() : new Date(dates_array[1].trim());
                         obj.issue_date = moment(start_date).format('YYYY-MM-DD');
                         obj.expiration_date =  moment(end_date).format('YYYY-MM-DD');
                     }
@@ -226,7 +226,8 @@ function clickedLeadAdd(res){
         editLead(res);
         shadowRoot.getElementById('btnBack').style.display = "block";
     });
-    shadowRoot.getElementById('refresh').addEventListener('click', () => {
+    // shadowRoot.getElementById('refresh').addEventListener('click', () => {
+    $(shadowRoot.getElementById('refresh')).off('click').on('click', () => {
         refresh();
     });
     chrome.runtime.sendMessage({call: "getAllList"}, function(response) {
@@ -492,7 +493,8 @@ const getProfileDetailsFromAPI = () => {
                                     editLead(profile_detail_res);
                                     shadowRoot.getElementById('btnBack').style.display = "block";
                                 });
-                                shadowRoot.getElementById('refresh').addEventListener('click', () => {
+                                // shadowRoot.getElementById('refresh').addEventListener('click', () => {
+                                $(shadowRoot.getElementById('refresh')).off('click').on('click', () => {
                                     refresh();
                                 });
                                 chrome.runtime.sendMessage({call: "getAllList"}, function(response) {
@@ -727,26 +729,26 @@ function backDetails(res){
         shadowRoot.getElementById('companyToggleBtnBlock').style.display = "none";
     }
     // if(typeof res.otherSocialMedia === "object" && res.otherSocialMedia.length > 0){
-    if(res.otherSocialMedia.facebook !== undefined || res.otherSocialMedia.linkedin !== undefined || res.otherSocialMedia.instagram !== undefined || res.otherSocialMedia.twitter !== undefined){
+    if(res.otherSocialMedia.facebook !== null || res.otherSocialMedia.linkedin !== null || res.otherSocialMedia.instagram !== null || res.otherSocialMedia.twitter !== null){
         let ul_socialMedia = shadowRoot.getElementById('social_media');
         ul_socialMedia.innerHTML = null;
         var li = document.createElement('li');
-        if(res.otherSocialMedia.facebook !== undefined){
+        if(res.otherSocialMedia.facebook !== null){
             var li = document.createElement('li');
             li.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30.033" height="30.033" viewBox="0 0 30.033 30.033"><path id="Icon_awesome-facebook-square" data-name="Icon awesome-facebook-square" d="M26.816,2.25H3.218A3.218,3.218,0,0,0,0,5.468v23.6a3.218,3.218,0,0,0,3.218,3.218h9.2V22.073H8.2V17.267h4.223V13.6c0-4.166,2.48-6.468,6.28-6.468a25.587,25.587,0,0,1,3.722.324V11.55h-2.1a2.4,2.4,0,0,0-2.71,2.6v3.121h4.611l-.737,4.806H17.614V32.283h9.2a3.218,3.218,0,0,0,3.218-3.218V5.468A3.218,3.218,0,0,0,26.816,2.25Z" transform="translate(0 -2.25)" fill="#7e84a3"/></svg>&nbsp;&nbsp;'+res.otherSocialMedia.facebook;
             ul_socialMedia.appendChild(li);
         }
-        if(res.otherSocialMedia.linkedin !== undefined){
+        if(res.otherSocialMedia.linkedin !== null){
             var li = document.createElement('li');
             li.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="29.139" height="29.139" viewBox="0 0 29.139 29.139"><path id="Icon_awesome-linkedin" data-name="Icon awesome-linkedin" d="M27.057,2.25H2.075A2.09,2.09,0,0,0,0,4.351V29.288a2.09,2.09,0,0,0,2.075,2.1H27.057a2.1,2.1,0,0,0,2.081-2.1V4.351A2.1,2.1,0,0,0,27.057,2.25ZM8.807,27.226H4.488V13.32H8.813V27.226ZM6.647,11.421a2.5,2.5,0,1,1,2.5-2.5A2.505,2.505,0,0,1,6.647,11.421ZM25,27.226H20.677V20.462c0-1.613-.033-3.688-2.244-3.688-2.25,0-2.6,1.756-2.6,3.571v6.881H11.519V13.32h4.143v1.9h.059a4.549,4.549,0,0,1,4.091-2.244C24.183,12.975,25,15.857,25,19.6Z" transform="translate(0 -2.25)" fill="#7e84a3"/></svg>&nbsp;&nbsp;'+res.otherSocialMedia.linkedin;
             ul_socialMedia.appendChild(li);
         }
-        if(res.otherSocialMedia.instagram !== undefined){
+        if(res.otherSocialMedia.instagram !== null){
             var li = document.createElement('li');
             li.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" fill="#7e84a3"/></svg>&nbsp;&nbsp;'+res.otherSocialMedia.instagram;
             ul_socialMedia.appendChild(li);
         }
-        if(res.otherSocialMedia.twitter !== undefined){
+        if(res.otherSocialMedia.twitter !== null){
             var li = document.createElement('li');
             li.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" fill="#7e84a3"/></svg>&nbsp;&nbsp;'+res.otherSocialMedia.twitter;
             ul_socialMedia.appendChild(li);
@@ -794,7 +796,8 @@ function backDetails(res){
         editLead(res);
         shadowRoot.getElementById('btnBack').style.display = "block";
     });
-    shadowRoot.getElementById('refresh').addEventListener('click', () => {
+    // shadowRoot.getElementById('refresh').addEventListener('click', () => {
+    $(shadowRoot.getElementById('refresh')).off('click').on('click', () => {
         refresh();
     });
     shadowRoot.querySelectorAll('.btn-thumbs-up').forEach(function(element) {
@@ -870,7 +873,8 @@ function editLead(res){
     }else{
         shadowRoot.getElementById('socialMediaBlock').style.display = "none"
     }
-    shadowRoot.getElementById('btnBack').addEventListener('click', (event) => {
+    // shadowRoot.getElementById('btnBack').addEventListener('click', (event) => {
+    $(shadowRoot.getElementById('btnBack')).off('click').on('click', (event) => {
         backDetails(res)
         event.target.style.display = "none";
     })
@@ -1363,7 +1367,8 @@ function accountDropdown(){
 }
 function showIntegrationPage(){
     shadowRoot.getElementById('drop_down').innerHTML = integrationHTML;
-        shadowRoot.getElementById('btnBack').addEventListener('click', () => {
+        // shadowRoot.getElementById('btnBack').addEventListener('click', () => {
+        $(shadowRoot.getElementById('btnBack')).off('click').on('click', (event) => {
             shadowRoot.getElementById('drop_down').innerHTML = dropdownContentHTML;
             getUserInfoFromAPI();
             shadowRoot.getElementById('btnIntegration').addEventListener('click', () => {
@@ -1434,7 +1439,8 @@ function refresh(){
                         editLead(res);
                         shadowRoot.getElementById('btnBack').style.display = "block";
                     });
-                    shadowRoot.getElementById('refresh').addEventListener('click', () => {
+                    // shadowRoot.getElementById('refresh').addEventListener('click', () => {
+                    $(shadowRoot.getElementById('refresh')).off('click').on('click', () => {
                         refresh();
                     });
                    
